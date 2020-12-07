bfd3487d9a8ddeb1e9fafc85091b9296261f56f7
Sanjit1 <sanjitsarda4@gmail.com>
2020-12-06 17:09:59 -0800

    Finished Book Scrapping: Final Code extracts books on hte home page and lists title, url, price and ratings

--- books-to-scrape/main.py
+++ books-to-scrape/main.py
@@ -5,4 +5,18 @@ from bs4 import BeautifulSoup, NavigableString
 home_page_url = "http://books.toscrape.com/index.html"
 home_page_request = requests.get(home_page_url)
 home_page_soup = BeautifulSoup(home_page_request.content, 'html.parser')
-book_list = home_page_soup.find_all('ol', class_='row')[0].children;
+book_element_list = home_page_soup.find_all('ol', class_='row')[0].children;
+book_object_list = []
+for book_element in book_element_list:
+    if isinstance(book_element, NavigableString):
+        continue
+    else:
+        book_object_list.append({
+            'title': list(book_element.find_all("h3")[0].children)[0]["title"], 
+            'url': "http://books.toscrape.com/" + list(book_element.find_all("h3")[0].children)[0]["href"],
+            'price': book_element.find_all("p", class_="price_color")[0].getText(),
+            'rating': book_element.find_all("p", class_="star-rating")[0]['class'][1] + "Stars"
+        })
+
+for book in book_object_list:
+    print(book)
